d8d1ba744fcf4a7de2e11d26aa58583bb8a50a62
Keith Filkins <kafilkins@me.com>
2020-11-24 00:46:42 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -12,8 +12,8 @@ def words_starting_with_un_and_ending_with_ing(text)
 end
 
 def words_five_letters_long(text)
-  text.grep(/w{5}/)
-
+  words = text.split(" ")
+  words.grep(/\b\w{5}\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
